f38584ee2caf95fb3081a72afebcc281f9f7abda
rahularya002 <ritiksain258@gmail.com>
2025-06-30 16:57:58 +0530

    fixed

--- old/app/admin/product-requests/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function AdminProductRequestsLoading() {
-  return (
-    <div className="flex items-center justify-center h-full">
-      <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-red-500"></div>
-    </div>
-  )
-}
